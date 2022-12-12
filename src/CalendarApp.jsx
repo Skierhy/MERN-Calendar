211611279998603e3b7373c8661261abbed24a82
Skierhy <Skierhy@gmail.com>
2022-12-12 13:56:07 -0600

    crear rutas

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router';
 import { store } from './store';
 
-
 export const CalendarApp = () => {
-  return (
-    <Provider store={ store }>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </Provider>
-  )
-}
+	return (
+		<Provider store={store}>
+			{/* necesita  BrowserRouter para usar las rutas*/}
+			<BrowserRouter>
+				{/* renderizar las rutas */}
+				<AppRouter />
+			</BrowserRouter>
+		</Provider>
+	);
+};
