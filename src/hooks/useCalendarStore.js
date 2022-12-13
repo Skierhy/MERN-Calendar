ed0ccef82dde8497ec648ec7c5f189c976c7a0da
Skierhy <Skierhy@gmail.com>
2022-12-12 19:01:37 -0600

    calendarslice

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,46 +1,49 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
-
+import {
+	onAddNewEvent,
+	onDeleteEvent,
+	onSetActiveEvent,
+	onUpdateEvent,
+} from '../store';
 
 export const useCalendarStore = () => {
-  
-    const dispatch = useDispatch();
-    const { events, activeEvent } = useSelector( state => state.calendar );
+	const dispatch = useDispatch();
+	const { events, activeEvent } = useSelector((state) => state.calendar);
 
-    const setActiveEvent = ( calendarEvent ) => {
-        dispatch( onSetActiveEvent( calendarEvent ) )
-    }
+	const setActiveEvent = (calendarEvent) => {
+		dispatch(onSetActiveEvent(calendarEvent));
+	};
 
-    const startSavingEvent = async( calendarEvent ) => {
-        // TODO: llegar al backend
+	const startSavingEvent = async (calendarEvent) => {
+		// TODO: llegar al backend
 
-        // Todo bien
-        if( calendarEvent._id ) {
-            // Actualizando
-            dispatch( onUpdateEvent({ ...calendarEvent }) );
-        } else {
-            // Creando
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
-        }
-    }
+		// Todo bien
+		if (calendarEvent._id) {
+			// Actualizando
+			dispatch(onUpdateEvent({ ...calendarEvent }));
+		} else {
+			// Creando
+			dispatch(
+				onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() })
+			);
+		}
+	};
 
-    const startDeletingEvent = () => {
-        // Todo: Llegar al backend
+	const startDeletingEvent = () => {
+		// Todo: Llegar al backend
 
+		dispatch(onDeleteEvent());
+	};
 
-        dispatch( onDeleteEvent() );
-    }
+	return {
+		//* Propiedades
+		activeEvent,
+		events,
+		hasEventSelected: !!activeEvent,
 
-
-    return {
-        //* Propiedades
-        activeEvent,
-        events,
-        hasEventSelected: !!activeEvent,
-
-        //* Métodos
-        startDeletingEvent,
-        setActiveEvent,
-        startSavingEvent,
-    }
-}
+		//* Métodos
+		startDeletingEvent,
+		setActiveEvent,
+		startSavingEvent,
+	};
+};
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,54 +1,54 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { addHours } from 'date-fns';
 
-const tempEvent =   {
-    _id: new Date().getTime(),
-    title: 'Cumpleaños del Jefe',
-    notes: 'Hay que comprar el pastel',
-    start: new Date(),
-    end: addHours( new Date(), 2 ),
-    bgColor: '#fafafa',
-    user: {
-      _id: '123',
-      name: 'Fernando'
-    }
+// Temporal event por que se va a leer del backend
+const tempEvent = {
+	_id: new Date().getTime(),
+	title: 'Cumpleaños del Jefe',
+	notes: 'Hay que comprar el pastel',
+	start: new Date(),
+	end: addHours(new Date(), 2),
+	bgColor: '#fafafa',
+	user: {
+		_id: '123',
+		name: 'Fernando',
+	},
 };
 
-
 export const calendarSlice = createSlice({
-    name: 'calendar',
-    initialState: {
-        events: [
-            tempEvent
-        ],
-        activeEvent: null
-    },
-    reducers: {
-        onSetActiveEvent: ( state, { payload }) => {
-            state.activeEvent = payload;
-        },
-        onAddNewEvent: ( state, { payload }) => {
-            state.events.push( payload );
-            state.activeEvent = null;
-        },
-        onUpdateEvent: ( state, { payload } ) => {
-            state.events = state.events.map( event => {
-                if ( event._id === payload._id ) {
-                    return payload;
-                }
+	name: 'calendar',
+	initialState: {
+		events: [tempEvent],
+		activeEvent: null,
+	},
+	reducers: {
+		onSetActiveEvent: (state, { payload }) => {
+			state.activeEvent = payload;
+		},
+		onAddNewEvent: (state, { payload }) => {
+			state.events.push(payload);
+			state.activeEvent = null;
+		},
+		onUpdateEvent: (state, { payload }) => {
+			state.events = state.events.map((event) => {
+				if (event._id === payload._id) {
+					return payload;
+				}
 
-                return event;
-            });
-        },
-        onDeleteEvent: ( state ) => {
-            if ( state.activeEvent ) {
-                state.events = state.events.filter( event => event._id !== state.activeEvent._id );
-                state.activeEvent = null;
-            }
-        }
-    }
+				return event;
+			});
+		},
+		onDeleteEvent: (state) => {
+			if (state.activeEvent) {
+				state.events = state.events.filter(
+					(event) => event._id !== state.activeEvent._id
+				);
+				state.activeEvent = null;
+			}
+		},
+	},
 });
 
-
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } =
+	calendarSlice.actions;
